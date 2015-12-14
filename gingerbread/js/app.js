c31b63e37ace02887871781e92eb78a3a6b802f1
Mallfurion <thomas_scout@yahoo.com>
2015-12-15 00:51:13 +0200

    final commit

--- gingerbread/js/app.js
+++ gingerbread/js/app.js
@@ -1,19 +1,37 @@
 var distancePerPoint = 10;
 var drawFPS          = 60;
 
-var orig = document.querySelector('path'), points, timer, timer2,
+//slow-scroll
+$(function() {
+    $('a[href*=#]:not([href=#])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
+});
+
+var orig = document.querySelector('path'), points, timer, timer2, timer3, done,
     canvas = document.querySelector('canvas'),
-    ctx=canvas.getContext('2d');
+    ctx=canvas.getContext('2d', {alpha: true});
 
 canvas.addEventListener('mouseover',startDrawingPath,false);
 canvas.addEventListener('mouseout', stopDrawingPath, false);
 
 function startDrawingPath(){
+    done = false;
     points = [];
     ctx.lineWidth = 15;
     ctx.strokeStyle = 'white';
     timer = setInterval(buildPath,1000/drawFPS);
     timer2 = setTimeout(paintMiddle, 3200);
+
 }
 
 function buildPath(){
@@ -34,6 +52,7 @@ function redrawCanvas(){
 }
 
 function paintMiddle(){
+    document.getElementById('selection').style.fill = '#a30';
     document.getElementById('center-path-eyes').style.fill = 'white';
     document.getElementById('center-path-mouth').style.fill = 'red';
     document.getElementById('center-path-bowtie').style.fill = 'green';
@@ -42,6 +61,8 @@ function paintMiddle(){
     //showText
     $('#merry').show('slide', 500);
     $('#christmas').show('slide', {direction: 'right'}, 500);
+    done = true;
+    generateImage();
 }
 
 function clearCanvas(){
@@ -58,9 +79,33 @@ function clearMiddle() {
     $('#christmas').hide('slide', {direction: 'right'}, 500);
 }
 
+function generateImage() {
+    html2canvas(document.getElementById('graphics'), {
+        onrendered: function(canvas) {
+            var img = canvas.toDataURL('image/jpg');
+            $('#saved-img').attr('src', img);
+        }
+    });
+
+    if(done)
+    {
+        canvas.removeEventListener('mouseover', startDrawingPath, false);
+        canvas.removeEventListener('mouseout', stopDrawingPath, false);
+        $('#saved-text').text('Your cookie is done! You can save it now.');
+        setTimeout(function(){document.getElementById('saved-anchor').click()}, 500);
+    }
+    else
+    {
+        $('#saved-text').text('Your cookie is NOT done. Let it cook!')
+    }
+
+}
+
 function stopDrawingPath(){
+    generateImage();
     clearInterval(timer);
     clearTimeout(timer2);
+    clearTimeout(timer3);
     clearCanvas();
     clearMiddle();
 }
